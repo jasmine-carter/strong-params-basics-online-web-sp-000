fe7370c9d920a26130d7e32269d6a3fadb16634f
Jasmine Carter <jasmine.carter87@gmail.com>
2020-10-04 19:25:33 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
 	end
 
 	def update
-	  @post = Post.find(params.require(:post).permit(:title))
+	  @post = Post.find(params[:id])
 	  @post.update(params["post"])
 	  redirect_to post_path(@post)
 	end
